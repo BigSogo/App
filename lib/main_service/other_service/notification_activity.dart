4741a7fb00999113f88dcb62c756bd1a76004a20
qpeterp <132974777+qpeterp@users.noreply.github.com>
2024-02-03 11:46:07 +0900

    Feat :: notificationView sentence 'will update'

--- lib/main_service/other_service/notification_activity.dart
+++ lib/main_service/other_service/notification_activity.dart
@@ -17,65 +17,72 @@ class _NotificationViewState extends State<NotificationView>{
           ),
         ),
       ),
-      body: Container(
-        height: 130,
-        margin: EdgeInsets.only(bottom: 10),
-        width: double.infinity,
-        color: Color(0xFFE0F3FF),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("Q. 씹덕이여도 성공할 수 있을까요..?", style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20
-                  ),),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("A. 애니를 시청하는 것은 개발자의 기본 소양이.....", style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16
-                  ),),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        // 버튼 클릭 시 수행할 동작 추가
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                        MaterialStateProperty.all<Color>(Color(0xFF4B66DC)),
-                        shape: MaterialStateProperty.all<OutlinedBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                        ),
-                      ),
-                      child: Text(
-                        "엄성민님의 답글 보기",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      body : Center(
+        child: Text("업데이트 예정입니다.", style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 20
+        ),),
+      )
+
+      // Container(
+      //   height: 130,
+      //   margin: EdgeInsets.only(bottom: 10),
+      //   width: double.infinity,
+      //   color: Color(0xFFE0F3FF),
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(8.0),
+      //     child: Column(
+      //       children: [
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.start,
+      //           children: [
+      //             Text("Q. 씹덕이여도 성공할 수 있을까요..?", style: TextStyle(
+      //               fontWeight: FontWeight.w600,
+      //               fontSize: 20
+      //             ),),
+      //           ],
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.start,
+      //           children: [
+      //             Text("A. 애니를 시청하는 것은 개발자의 기본 소양이.....", style: TextStyle(
+      //                 fontWeight: FontWeight.w600,
+      //                 fontSize: 16
+      //             ),),
+      //           ],
+      //         ),
+      //         Padding(
+      //           padding: const EdgeInsets.all(8.0),
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.end,
+      //             children: [
+      //               TextButton(
+      //                 onPressed: () {
+      //                   // 버튼 클릭 시 수행할 동작 추가
+      //                 },
+      //                 style: ButtonStyle(
+      //                   backgroundColor:
+      //                   MaterialStateProperty.all<Color>(Color(0xFF4B66DC)),
+      //                   shape: MaterialStateProperty.all<OutlinedBorder>(
+      //                     RoundedRectangleBorder(
+      //                       borderRadius: BorderRadius.circular(5.0),
+      //                     ),
+      //                   ),
+      //                 ),
+      //                 child: Text(
+      //                   "엄성민님의 답글 보기",
+      //                   style: TextStyle(
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
 
     );
   }
